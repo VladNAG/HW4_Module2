564ec4023d28f4a436f42e973c3edcc447b5f616
Vladyslav Nahornyi <vladnagornyj2@gmail.com>
2023-06-05 20:05:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HW4_Module2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
